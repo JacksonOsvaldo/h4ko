413aac605b5ae06efd555dd0bd70b6c4c3a35fdd
JacksonOsvaldo <jacksonosvaldo@live.com>
2024-11-07 14:39:20 -0300

    feat: update arch

--- h4ko/cli.py
+++ h4ko/cli.py
@@ -7,20 +7,22 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates', 'project_tem
 
 @click.group()
 def main():
-    """Comando principal para o RPA Framework"""
     pass
 
 @main.command()
 @click.argument('name')
 def startproject(name):
-    """Cria um novo projeto RPA"""
-    os.makedirs(name, exist_ok=True)
-    shutil.copytree(TEMPLATE_DIR, os.path.join(name), dirs_exist_ok=True)
-    click.echo(f'Projeto {name} criado com sucesso.')
+    if name == '.':
+        shutil.copytree(TEMPLATE_DIR, os.path.join(name), dirs_exist_ok=True)
+        click.echo(f'Projeto {name} criado com sucesso.')
+    else:
+        os.makedirs(name, exist_ok=True)
+        shutil.copytree(TEMPLATE_DIR, os.path.join(name), dirs_exist_ok=True)
+        click.echo(f'Projeto {name} criado com sucesso.')
+   
 
 @main.command()
 def initdb():
-    """Inicializa o banco de dados"""
     init_db()
     click.echo('Banco de dados inicializado com sucesso.')
 
--- setup.py
+++ setup.py
@@ -2,20 +2,30 @@ from setuptools import setup, find_packages
 
 setup(
     name='h4ko',
-    author='Jackson Osvaldo',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click',
-        'SQLAlchemy',
+        'click',        
+        'SQLAlchemy',   
     ],
     package_data={
-        'h4ko': ['templates/project_template/*'],
+        'h4ko': ['templates/project_template/**/*'],
     },
     entry_points={
         'console_scripts': [
             'h4ko=h4ko.cli:main',
         ],
     },
+    author='Jackson Osvaldo',
+    description='Descrição do seu pacote RPA framework',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/JacksonOsvaldo/h4ko',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
 )
